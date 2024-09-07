302e4c9ed728719635d0b9b108042512a453169f
Uriel Montes <brianmontes739@gmail.com>
2024-09-06 19:03:15 -0600

    primer commit

---
+++ src/temas-ts/01-tiposDat.ts
@@ -0,0 +1,21 @@
+
+var num1=38
+var num2=29
+const num3=10
+
+let sum = num1+num2+num3;
+
+let nombre: string
+let num:number
+let activo:boolean
+
+nombre = 'Drimzzy'
+num=27
+activo = true
+
+let matricula:number|string|boolean
+
+matricula=1234567
+matricula='aSDAS'
+
+console.log("el número es", sum)
---
+++ src/temas-ts/02-interfaz.ts
@@ -0,0 +1,12 @@
+interface Alumno{
+    nombre:string;
+    edad:number;
+    email:string;
+    calificacion?:number;
+}
+
+const alumno:Alumno={
+    nombre: 'Drimzzy',
+    edad: 25,
+    email: 'uriel@gmail.com',
+}
